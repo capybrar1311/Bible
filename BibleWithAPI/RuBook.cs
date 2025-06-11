2cb7c7c0b3c40c0ea6de2af7186cb3c4b5d5955d
Capy_Clollege <lyaminlosha@yandex.ru>
2025-06-11 14:20:59 +0300

    last in college

--- BibleWithAPI/RuBook.cs
+++ BibleWithAPI/RuBook.cs
@@ -2,13 +2,11 @@
 
 public class RuBook : List<RuChapter>
 {
-    public static List<RuChapter> book = new List<RuChapter>();
-    public string bookName =  book[0].bookName;
-    
     
     public static List<RuBook> Makebooks(List<RuChapter> chapters)
     {
         var allbk = new List<RuBook>();
+        List<RuChapter> book = new List<RuChapter>();
         RuBook currentBook = new RuBook();
         for (int i = 0; i < chapters.Count-1; i++)
         {
@@ -18,7 +16,7 @@ public class RuBook : List<RuChapter>
             }
             else if (chapters[i].bookName != chapters[i+1].bookName)
             {
-                RuBook.book.Add(chapters[i]);
+                book.Add(chapters[i]);
                 RuBible.allBooks.Add(currentBook);
                 currentBook = new RuBook();
             }
