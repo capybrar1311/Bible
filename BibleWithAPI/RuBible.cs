87d7d0f924a41aad5457d243d75e02a24c537c03
CapybaraHome <lyaminlosha@yandex.ru>
2025-06-22 23:25:59 +0300

    finally it works

--- BibleWithAPI/RuBible.cs
+++ BibleWithAPI/RuBible.cs
@@ -1,16 +1,29 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BibleWithAPI;
 
 public class RuBible
 {
-    public Dictionary<string, string> Verses { get; set; }
+    [Newtonsoft.Json.JsonExtensionData]
+    public required Dictionary<string, JToken> Verses { get; set; }
     [JsonProperty("info")]
-    public RuInfo Info { get; set; }
-
-    public RuBible()
-    {
-        Verses = new Dictionary<string, string>();
-        Info = new RuInfo();
-    }
+    public required RuInfo Info { get; set; }
 }
+
+public class RuInfo
+{
+    [JsonPropertyName("translation")]
+    public required string Translation { get; set; }
+
+    [JsonPropertyName("book")]
+    public required string Book { get; set; }
+
+    [JsonPropertyName("chapter")]
+    public int Chapter { get; set; }
+
+    [JsonPropertyName("verse")]
+    public required string Verse { get; set; }
+}
+
--- BibleWithAPI/RuBibleManager.cs
+++ BibleWithAPI/RuBibleManager.cs
@@ -1,16 +1,85 @@
-﻿using System.Text.Json;
+﻿
+using Newtonsoft.Json;
 
 namespace BibleWithAPI;
 
 public class RuBibleManager
 {
-    
+    public static readonly Dictionary<string, int> SynodalBooks = new Dictionary<string, int>
+    {
+        {"Бытие", 1},
+        {"Исход", 2},
+        {"Левит", 3},
+        {"Числа", 4},
+        {"Второзаконие", 5},
+        {"Иисус Навин", 6},
+        {"Судьи", 7},
+        {"Руфь", 8},
+        {"1 Царств", 9},
+        {"2 Царств", 10},
+        {"3 Царств", 11},
+        {"4 Царств", 12},
+        {"1 Паралипоменон", 13},
+        {"2 Паралипоменон", 14},
+        {"Ездра", 15},
+        {"Неемия", 16},
+        {"Есфирь", 17},
+        {"Иов", 18},
+        {"Псалмы", 19},
+        {"Притчи", 20},
+        {"Екклезиаст", 21},
+        {"Песнь Песней", 22},
+        {"Исаия", 23},
+        {"Иеремия", 24},
+        {"Плач Иеремии", 25},
+        {"Иезекиил", 26},
+        {"Даниил", 27},
+        {"Осия", 28},
+        {"Иоиль", 29},
+        {"Амос", 30},
+        {"Авдий", 31},
+        {"Иона", 32},
+        {"Михей", 33},
+        {"Наум", 34},
+        {"Аввакум", 35},
+        {"Софония", 36},
+        {"Аггей", 37},
+        {"Захария", 38},
+        {"Малахия", 39},
+        {"Матфей", 40},
+        {"Марк", 41},
+        {"Лука", 42},
+        {"Иоанн", 43},
+        {"Деяния", 44},
+        {"Иаков", 45},
+        {"1 Петра", 46},
+        {"2 Петра", 47},
+        {"1 Иоанна", 48},
+        {"2 Иоанна", 49},
+        {"3 Иоанна", 50},
+        {"Иуда", 51},
+        {"Римлянам", 52},
+        {"1 Коринфянам", 53},
+        {"2 Коринфянам", 54},
+        {"Галатам", 55},
+        {"Ефесянам", 56},
+        {"Филиппийцам", 57},
+        {"Колоссянам", 58},
+        {"1 Фессалоникийцам", 59},
+        {"2 Фессалоникийцам", 60},
+        {"1 Тимофею", 61},
+        {"2 Тимофею", 62},
+        {"Титу", 63},
+        {"Филимону", 64},
+        {"Евреям", 65},
+        {"Откровение", 66}
+    };
 
     public static async Task GetData(string userInput)
     {
         try
         {
-            string requestUrl = $"https://justbible.ru/api/bible?translation=nasb&book=43&chapter=3&verses=16";
+            string requestUrl = $"https://justbible.ru/api/bible?translation=rst&book=43&chapter=1&verses=1,3,14";
             
             var response = await Menu.client.GetStringAsync(requestUrl);
 
@@ -27,12 +96,14 @@ public class RuBibleManager
     
     private static void PrintTheVerses(RuBible vvv)
     {
-        Console.WriteLine(vvv.Info.Verse);
+        foreach (var verse in vvv.Verses)
+        {
+            Console.WriteLine($"{vvv.Info.Book} {vvv.Info.Chapter}:{verse.Key} {verse.Value}");
+        }
     }
     
     private static RuBible ParseFullJson(string json)
     {
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        return JsonSerializer.Deserialize<RuBible>(json, options);
+        return JsonConvert.DeserializeObject<RuBible>(json);
     }
 }
